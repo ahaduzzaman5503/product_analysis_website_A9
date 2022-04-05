615b1d1b60bfe6a9bba00d248a45b8fddc969056
ahaduzzaman550 <ahaduzzaman5503@gmail.com>
2022-04-05 15:41:05 +0600

    9 review show in 3 coloum on review section

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Programming bigginer book project!
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Code link:
+# Code link:
 
 https://github.com/programming-hero-web-course-4/product-analysis-website-ahaduzzaman5503
 
-### Live Website link:
+# Live Website link:
 
 https://dashing-chebakia-1eb621.netlify.app/
 
@@ -32,20 +31,3 @@ This book begginer frendly.
 
 ### Analyzing the Bundle Size
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/Components/ReviewShow/ReviewShow.js
+++ src/Components/ReviewShow/ReviewShow.js
@@ -4,29 +4,27 @@ import "./ReviewShow.css"
 const ReviewShow = (props) => {
     const { reviewerName, rating, userImg, reviewComment } = props.review;
     return (
-        <div className='container-display'>
-            <div className="single-review">
-
-                <div className="image-section">
-                    <img src={userImg} alt="" />
+        <div className="single-review">
+            <div className="name-ratting">
+            <div className="image-section">
+                <img src={userImg} alt="" />
+            </div>
+            <div className="name-rating">
+                <div className="name">
+                    <h3><small>by:</small> {reviewerName}</h3>
                 </div>
-                <div className="name-rating">
-                    <div className="name">
-                        <h3><small>by:</small> {reviewerName}</h3>
-                    </div>
-                    
-                    <div className="star-ratting">
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>  {rating}
-                        <div className="comment">
-                            {/* { reviewComment.length < 100 ? : reviewComment.slice(0, 100) + "<span className='fw-bold'>... read more</span>"} */}
-                            {reviewComment}
-                        </div>
-                    </div>
+                <div className="star-ratting">
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>  {rating}
                 </div>
+            </div>
+            </div>
+            <div className="comment">
+                {/* { reviewComment.length < 100 ? : reviewComment.slice(0, 100) + "<span className='fw-bold'>... read more</span>"} */}
+                {reviewComment}
             </div>
         </div>
     );
