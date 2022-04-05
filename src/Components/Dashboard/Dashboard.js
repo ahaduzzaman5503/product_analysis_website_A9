bcfd5ea9dbdbc4eb3affb5ddc958a41bfda93d36
ahaduzzaman550 <ahaduzzaman5503@gmail.com>
2022-04-05 20:07:08 +0600

    add two chart and some edit on review section

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import "./Dashboard.css"
 
 const Dashboard = () => {
@@ -42,10 +42,10 @@ const Dashboard = () => {
         }
     ]
     return (
-        <div>
-            <h1> This is Deshbord Chart Page</h1>
+        <div className='allChart'>
+            
 
-            <div className="twoChart">
+            <div className="oneChart">
                 <BarChart width={600} height={300} data={data}>
                     <XAxis dataKey="month" />
                     <YAxis />
@@ -54,13 +54,47 @@ const Dashboard = () => {
                 </BarChart>
             </div>
 
-            <div className="oneChart">
+            <div className="twoChart">
                 <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                     <Line type="monotone" dataKey="month" stroke="#8884d8"></Line>
                     <XAxis dataKey="month"></XAxis>
                     <CartesianGrid stroke="#ccc" strokeDasharray="5 5"></CartesianGrid>
                     <YAxis dataKey="sell"></YAxis>
                     <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <div className="threeChart">
+                <AreaChart width={600} height={300} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="sell" />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="month" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="sell" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </div>
+
+            <div className="fourChart">
+                <LineChart width={600} height={300} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="month" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
                 </LineChart>
             </div>
         </div>
--- src/Components/ReviewShow/ReviewShow.js
+++ src/Components/ReviewShow/ReviewShow.js
@@ -4,7 +4,21 @@ import "./ReviewShow.css"
 const ReviewShow = (props) => {
     const { reviewerName, rating, userImg, reviewComment } = props.review;
     return (
+        <div className="main">
+            <div className="about-review">
+            <div className="book-image">
+                <img src="https://ds.rokomari.store/rokomari110/ProductNew20190903/260X372/00ea58560_112222.jpg" alt="Book" />
+            </div>
+            <div className="book-writer">
+                <h2>হাবলুদের জন্য প্রোগ্রামিং</h2>
+                <h3>ঝংকার মাহবুব</h3>
+            </div>
+            <div className="image-user">
+                    <img src="https://ds.rokomari.store/rokomari110/people/f05844bf5ab4_40266.jpg" alt="" />
+            </div>
+            </div>
         <div className="single-review">
+            
             <div className="name-ratting">
             <div className="image-section">
                 <img src={userImg} alt="" />
@@ -26,6 +40,7 @@ const ReviewShow = (props) => {
                 {/* { reviewComment.length < 100 ? : reviewComment.slice(0, 100) + "<span className='fw-bold'>... read more</span>"} */}
                 {reviewComment}
             </div>
+        </div>
         </div>
     );
 };
